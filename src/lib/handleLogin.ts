9ffa6b0073e4f25e108463009e5f1a38b3971c00
WavyBoyJodii <najeecasey@gmail.com>
2024-09-06 20:26:22 -0400

    create function to handle login

---
+++ src/lib/handleLogin.ts
@@ -0,0 +1,18 @@
+import { getUsers } from './supabaseAdmin';
+
+export const handleLogin = async (username: string, password: string) => {
+  const users = await getUsers();
+  // Check if user exists
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return { error: true, reason: 'No such user exists' };
+  }
+
+  // Check password
+  if (user.password !== password) {
+    return { error: true, reason: 'Incorrect password' };
+  }
+
+  // If both match, return success and the user object
+  return { success: true, user };
+};
