6c4fc88410c1e2783d4dc1bca081490aa87c2ad0
WavyBoyJodii <najeecasey@gmail.com>
2024-09-06 21:16:46 -0400

    fix env variable name

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -6,7 +6,7 @@ import { Database } from '@/types_db';
 
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_ANON_KEY || ''
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 );
 
 const getUsers = async (): Promise<User[]> => {
