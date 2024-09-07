bbe7dad89e72cad31e2c764dc7a648063a9de59f
WavyBoyJodii <najeecasey@gmail.com>
2024-09-06 20:28:37 -0400

    create function to clear cookies

---
+++ src/lib/clearAuthAndUser.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export default async function clearAuthCookie() {
+  console.log(`logging clear auth cookie.....`);
+  cookies().set('auth', '');
+  cookies().set('user', '');
+  console.log(`auth cookie should be cleared...`);
+}
