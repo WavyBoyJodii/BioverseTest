83a5c759c017f036eb9c21451a9b2e0978139959
WavyBoyJodii <najeecasey@gmail.com>
2024-09-08 16:02:45 -0400

    update admin page in app router

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,32 +1,9 @@
-'use client';
+'use server';
 
-import { Button } from '@/components/ui/button';
-import clearAuthCookie from '@/lib/clearAuthAndUser';
-import { useRouter } from 'next/navigation';
-import { toast } from 'sonner';
-import { LogOut } from 'lucide-react';
+import AdminPanel from '@/components/admin-panel';
+import { getUserById } from '@/lib/supabaseAdmin';
 
-export default function Admin() {
-  const router = useRouter();
-  const handleLogout = async () => {
-    await clearAuthCookie();
-    toast('Logging out User');
-    setTimeout(() => {
-      router.refresh();
-    }, 2000);
-  };
-  return (
-    <div>
-      <p>No admin page yet</p>
-      <Button
-        variant="outline"
-        size="sm"
-        className="flex items-center space-x-2"
-        onClick={handleLogout}
-      >
-        <LogOut className="h-4 w-4" />
-        <span>Logout</span>
-      </Button>
-    </div>
-  );
+export default async function Admin() {
+  const user = await getUserById();
+  return <AdminPanel user={user} />;
 }
