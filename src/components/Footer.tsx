2bf44b05060e69c5aab5631ed0f1fa46357b67ae
WavyBoyJodii <najeecasey@gmail.com>
2024-09-08 16:26:12 -0400

    create footer component for consistency

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mt-auto py-4 text-center text-sm text-green-800">
+      © 2024 Bioverse. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
