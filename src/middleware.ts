dddbaeed51300d6c17cbd814989aa6e6209deeb4
WavyBoyJodii <najeecasey@gmail.com>
2024-09-08 16:28:15 -0400

    remove logs from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,7 @@ export const config = {
 };
 
 export default async function middleware(req: NextRequest) {
-  console.log(`running middleware...`);
   const valid = await getAuth();
-  console.log(`logging middleware auth response ${valid}`);
   if (valid === false) {
     return NextResponse.redirect(new URL('/', req.url));
   } else {
